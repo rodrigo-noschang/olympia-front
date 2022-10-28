5fd8c6d1ef7115ed9906972731a458a5b05c6c4f
Rodrigo Noschang <rodrigo@hotmail.com>
2022-10-28 16:55:27 -0300

    redirects to dashboard if user is already loged in, and redirects do register/login if he is not

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,8 @@
 import HeaderContainer from "./style";
 import { AiOutlineLogout } from 'react-icons/ai';
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import jwt_decode from 'jwt-decode';
 
 const Header = () => {
     const navigate = useNavigate();
@@ -10,6 +12,13 @@ const Header = () => {
         localStorage.removeItem('diet-buddy:token');
         navigate('/');
     }
+
+    useEffect(() => {
+        if (token) {
+            const { user_id } = jwt_decode(token)
+            navigate(`/dashboard/${user_id}`)
+        }
+    }, [])
 
     return (
         <HeaderContainer>
